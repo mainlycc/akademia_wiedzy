3d620b992803ead556a375d52b12f2b0e3dbba2e
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-09-10 19:26:07 +0200

    nelson

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -664,7 +664,7 @@ interface TutorFromDB {
 interface StudentParentRelation {
   is_primary: boolean
   relation: 'mother' | 'father' | 'guardian' | 'other'
-  parents: ParentFromDB
+  parents: ParentFromDB[]
 }
 
 interface EnrollmentFromDB {
@@ -783,16 +783,18 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
 
       // Przekształć dane rodziców na format oczekiwany przez StudentCard
       const parentsInfo = (parents as StudentParentRelation[] || [])
-        .filter((sp) => sp.parents)
-        .map((sp) => ({
-          id: sp.parents.id,
-          first_name: sp.parents.first_name,
-          last_name: sp.parents.last_name,
-          email: sp.parents.email,
-          phone: sp.parents.phone,
-          relation: sp.relation,
-          is_primary: sp.is_primary,
-        }))
+        .filter((sp) => sp.parents && sp.parents.length > 0)
+        .flatMap((sp) => 
+          sp.parents.map((parent) => ({
+            id: parent.id,
+            first_name: parent.first_name,
+            last_name: parent.last_name,
+            email: parent.email,
+            phone: parent.phone,
+            relation: sp.relation,
+            is_primary: sp.is_primary,
+          }))
+        )
 
       // Przekształć dane przedmiotów na format oczekiwany przez StudentCard
       const subjectsInfo = (enrollments as EnrollmentFromDB[] || []).map((enrollment) => ({
